5e763769159165336967308f1cd07129d277772d
Real-Partha <parthapratimpaul2003@gmail.com>
2023-06-29 12:38:17 +0530

    Added URL Model

--- urlshortner/models.py
+++ urlshortner/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Url(models.Model):
+    original_link = models.URLField(max_length=1000)
+    shortened_link = models.CharField(max_length=15)
+    time_created = models.DateTimeField(auto_now_add=True)
+    access_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.link
